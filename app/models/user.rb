ba839e180bef2511a9946d189a9f2acfddce5f69
Benjamin Rosas <ben@aliencyb.org>
2020-06-07 13:02:59 -0500

    user model and spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@
 class User < ApplicationRecord
   has_many :interests, dependent: :destroy
   has_many :activities, through: :interests
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,74 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be created by factory" do
+    create(:user)
+
+    expect(User.count).to be(1)
+  end
+
+  # it "must have password_confirmation" do
+  #   expect {
+  #     create(:user, password_confirmation: nil)
+  #   }.to raise_error(ActiveRecord::RecordInvalid)
+  # end
+
+  # it "must have a matching password_confirmation" do
+  #   expect {
+  #     create(:user, password: "secretsauce", password_confirmation: "ooppwhat")
+  #   }.to raise_error(ActiveRecord::RecordInvalid)
+  # end
+
+  it "must have a valid email" do
+    expect {
+      create(:user, email: nil)
+    }.to raise_error(ActiveRecord::RecordInvalid)
+
+    expect {
+      create(:user, email: "not_email_address")
+    }.to raise_error(ActiveRecord::RecordInvalid)
+
+    expect {
+      create(:user, email: "@bbc.co.uk")
+    }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "must have a unique email" do
+    create(:user, email: "original@test.co")
+
+    expect {
+      create(:user, email: "original@test.co")
+    }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  # it "is valid with only email and password" do
+  #   User.create(
+  #     email: "valid@email",
+  #     password: "verycryptic",
+  #     password_confirmation: "verycryptic"
+  #   )
+
+  #   expect(User.count).to be(1)
+  # end
+
+  # it "is not admin by default" do
+  #   user = User.create(
+  #     email: "valid@email",
+  #     password: "verycryptic",
+  #     password_confirmation: "verycryptic"
+  #   )
+
+  #   expect(user.is_admin).to be(false)
+  # end
+
+  # it "can be an admin" do
+  #   user = User.create(
+  #     email: "valid@email",
+  #     is_admin: true,
+  #     password: "verycryptic",
+  #     password_confirmation: "verycryptic"
+  #   )
+
+  #   expect(user.is_admin).to be(true)
+  # end
 end
