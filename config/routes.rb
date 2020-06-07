2f0e716710ffdb8814b08815dd01c0b4a39e36b9
Benjamin Rosas <ben@aliencyb.org>
2020-06-07 12:03:22 -0500

    use jsonapi routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :activities
-  resources :interests
-  resources :tags
-  resources :users
+  jsonapi_resources :activities
+  jsonapi_resources :interests
+  jsonapi_resources :tags
+  jsonapi_resources :users
 end
