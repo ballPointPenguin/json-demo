dad0085ea9fafa169e9fbbad9c595504420c0688
Benjamin Rosas <ben@aliencyb.org>
2020-06-08 01:49:11 -0500

    patch request spec

--- spec/requests/tags_request_spec.rb
+++ spec/requests/tags_request_spec.rb
@@ -4,4 +4,38 @@ require "rails_helper"
 
 RSpec.describe "Tags", type: :request do
   it_behaves_like "jsonapi request", Tag
+
+  let (:model) { Tag }
+  let (:model_name) { model.model_name }
+  let (:name_sym) { model_name.singular.to_sym }
+
+  describe "PATCH#update" do
+    subject(:make_request) do
+      patch "/#{model_name.route_key}/#{record.id}", params: params, as: :json
+    end
+
+    describe "update the title" do
+      let (:record) { create(name_sym) }
+
+      let (:params) do
+        {
+          "data": {
+            "id": record.id.to_s,
+            "type": model_name.collection,
+            "attributes": {
+              title: "new title"
+            }
+          }
+        }
+      end
+
+
+      it "updates the resource" do
+        make_request
+
+        expect(response).to have_http_status(:success)
+        expect(record.reload.title).to eq("new title")
+      end
+    end
+  end
 end
--- spec/shared_examples/requests/jsonapi_request.rb
+++ spec/shared_examples/requests/jsonapi_request.rb
@@ -5,13 +5,12 @@ RSpec.shared_examples "jsonapi request" do |model|
   let (:name_sym) { model_name.singular.to_sym }
 
   describe "GET#index" do
-    let (:params) { }
-
     subject(:make_request) do
       get "/#{model_name.route_key}", params: params, as: :json
     end
 
     describe "basic fetch index" do
+      let (:params) { }
       let!(:record1) { create(name_sym) }
       let!(:record2) { create(name_sym) }
       let(:record_ids) { [record1, record2].map { |record| record[:id].to_s } }
@@ -33,13 +32,12 @@ RSpec.shared_examples "jsonapi request" do |model|
   end
 
   describe "GET#show" do
-    let (:params) { }
-
     subject(:make_request) do
       get "/#{model_name.route_key}/#{record.id}", params: params, as: :json
     end
 
     describe "basic fetch record" do
+      let (:params) { }
       let!(:record) { create(name_sym) }
 
       it "returns a jsonapi object" do
@@ -56,23 +54,23 @@ RSpec.shared_examples "jsonapi request" do |model|
   end
 
   describe "POST#create" do
-    let (:attrs)  { attributes_for(name_sym) }
-    let (:related) { attributes_for_related(name_sym) }
-    let (:params) do
-      {
-        "data": {
-          "type": model_name.collection,
-          "attributes": attrs,
-          "relationships": related
-        }
-      }
-    end
-
     subject(:make_request) do
       post "/#{model_name.route_key}", params: params, as: :json
     end
 
     describe "basic create record" do
+      let (:attrs)  { attributes_for(name_sym) }
+      let (:related) { attributes_for_related(name_sym) }
+      let (:params) do
+        {
+          "data": {
+            "type": model_name.collection,
+            "attributes": attrs,
+            "relationships": related
+          }
+        }
+      end
+
       it "creates a database record" do
         expect { make_request }.to change { model.count }.by(1)
         expect(response).to have_http_status(:created)
