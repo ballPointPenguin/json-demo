5bfcbac50505abf0f126a1212df564cf58bcaae1
Benjamin Rosas <ben@aliencyb.org>
2020-06-14 20:06:44 -0500

    rejigger token auth config

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth", defaults: { format: "json" }
+  mount_devise_token_auth_for "User", at: "auth"
+
   as :user do
     # Define routes for User within this block.
   end
